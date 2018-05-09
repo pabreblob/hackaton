5c4c9e3ec2931c0897deee9fb0e1d798d487b8b7
Daniel <danielcarpioc@gmail.com>
2018-05-09 22:15:17 +0200

    Creados los controladores de SpamWord

---
+++ Acme-Taxi/src/main/java/converters/SpamWordToStringController.java
@@ -0,0 +1,23 @@
+
+package converters;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import domain.SpamWord;
+
+@Component
+@Transactional
+public class SpamWordToStringController implements Converter<SpamWord, String> {
+
+	@Override
+	public String convert(final SpamWord spamword) {
+		String res;
+		if (spamword == null)
+			res = null;
+		else
+			res = String.valueOf(spamword.getId());
+		return res;
+	}
+}
