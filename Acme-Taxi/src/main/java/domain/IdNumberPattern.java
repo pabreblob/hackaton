5f3b133eed96a5c9be54e948ab4c022a8bd7fdaf
Adrián Cantón Fernández <adrcanfer@gmail.com>
2018-05-10 18:29:22 +0200

    Quitado los uniques que fallaban

--- Acme-Taxi/src/main/java/domain/IdNumberPattern.java
+++ Acme-Taxi/src/main/java/domain/IdNumberPattern.java
@@ -5,8 +5,6 @@ import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
 
 import org.hibernate.validator.constraints.NotBlank;
 import org.hibernate.validator.constraints.SafeHtml;
@@ -14,9 +12,6 @@ import org.hibernate.validator.constraints.SafeHtml.WhiteListType;
 
 @Entity
 @Access(AccessType.PROPERTY)
-@Table(uniqueConstraints = {
-	@UniqueConstraint(columnNames = "pattern")
-})
 public class IdNumberPattern extends DomainEntity {
 
 	private String	pattern;
--- Acme-Taxi/src/main/java/domain/SpamWord.java
+++ Acme-Taxi/src/main/java/domain/SpamWord.java
@@ -5,8 +5,6 @@ import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
 
 import org.hibernate.validator.constraints.NotBlank;
 import org.hibernate.validator.constraints.SafeHtml;
@@ -14,9 +12,9 @@ import org.hibernate.validator.constraints.SafeHtml.WhiteListType;
 
 @Entity
 @Access(AccessType.PROPERTY)
-@Table(uniqueConstraints = {
-	@UniqueConstraint(columnNames = "word")
-})
+//@Table(uniqueConstraints = {
+//	@UniqueConstraint(columnNames = "word")
+//})
 public class SpamWord extends DomainEntity {
 
 	private String	word;
