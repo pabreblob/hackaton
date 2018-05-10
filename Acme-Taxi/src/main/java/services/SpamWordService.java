796a913cb81e740b1e7b8549d3085b2f1ef39815
Daniel <danielcarpioc@gmail.com>
2018-05-10 19:48:10 +0200

    Converters, repositorio y servicios de SpamWord

---
+++ Acme-Taxi/src/main/java/services/SpamWordService.java
@@ -0,0 +1,64 @@
+
+package services;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
+
+import repositories.SpamWordRepository;
+import domain.SpamWord;
+
+@Service
+@Transactional
+public class SpamWordService {
+
+	@Autowired
+	private SpamWordRepository	spamWordRepository;
+
+
+	//TODO: Quitar comentarios cuando se incorpore los servicios de Admin
+
+	//@Autowired
+	//private AdminService adminService;
+
+	public SpamWord create() {
+		//Assert.notNull(this.adminService.findByPrincipal());
+		final SpamWord res = new SpamWord();
+
+		return res;
+	}
+
+	public Collection<SpamWord> findAll() {
+		return this.spamWordRepository.findAll();
+	}
+
+	public SpamWord save(final SpamWord sw) {
+		Assert.notNull(sw);
+		Assert.notNull(sw.getWord());
+		Assert.isTrue(sw.getWord().replace(" ", "").length() != 0);
+		sw.setWord(sw.getWord().trim());
+		final SpamWord res = this.spamWordRepository.save(sw);
+		return res;
+	}
+
+	public void addWords(final String words) {
+		final String[] w = words.split(",");
+		SpamWord temp;
+		for (final String word : w) {
+			temp = this.create();
+			temp.setWord(word);
+			this.save(temp);
+		}
+	}
+
+	public void delete(final SpamWord sw) {
+		Assert.notNull(sw);
+		Assert.notNull(sw.getWord());
+		Assert.isTrue(sw.getId() > 0);
+		//Assert.notNull(this.adminService.findByPrincipal());
+		this.spamWordRepository.delete(sw);
+	}
+}
