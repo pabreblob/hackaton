a60ff1806fcf6546c99f083ab16c19b69e8e43f8
Pablo Narváez <peluconus@hotmail.com>
2018-05-11 18:57:46 +0200

    Solucionada query

--- Acme-Taxi/src/main/java/repositories/CommentRepository.java
+++ Acme-Taxi/src/main/java/repositories/CommentRepository.java
@@ -15,7 +15,10 @@ public interface CommentRepository extends JpaRepository<Comment, Integer> {
 	@Query("select c from Comment c where c.creator.id = ?1")
 	Collection<Comment> findCommentsByUserId(int userId);
 
-	@Query("select c from Announcement a join a.comments c where a.id=?1 order by c.moment DESC")
+	@Query("select c from Comment c where c.announcement.id=?1 order by c.moment DESC")
 	Collection<Comment> findCommentsOrdered(int announcementId);
+
+	@Query("select c from Comment c where c.comment.id=?1 order by c.moment DESC")
+	Collection<Comment> findCommentsByParentId(int commentId);
 
 }
