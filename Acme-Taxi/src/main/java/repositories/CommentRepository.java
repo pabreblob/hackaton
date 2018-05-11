7315e3deda8d6bfad5c8b4789bd762068ee8966f
Pablo Narváez <peluconus@hotmail.com>
2018-05-11 18:49:14 +0200

    Arreglado fallos

--- Acme-Taxi/src/main/java/repositories/CommentRepository.java
+++ Acme-Taxi/src/main/java/repositories/CommentRepository.java
@@ -12,10 +12,10 @@ import domain.Comment;
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
 
-	@Query("select c from Comment c where c.author.id = ?1")
+	@Query("select c from Comment c where c.creator.id = ?1")
 	Collection<Comment> findCommentsByUserId(int userId);
 
-	@Query("select c from Rendezvous r join r.comments c where r.id=?1 order by c.moment DESC")
-	Collection<Comment> findCommentsOrdered(int rendezvousId);
+	@Query("select c from Announcement a join a.comments c where a.id=?1 order by c.moment DESC")
+	Collection<Comment> findCommentsOrdered(int announcementId);
 
 }
