d2743273d3c05bec7133797db24abd7678e58082
pabreblob <pabreblob@alum.us.es>
2018-05-15 18:37:27 +0200

    Service y repository basico de user

---
+++ Acme-Taxi/src/main/java/repositories/UserRepository.java
@@ -0,0 +1,16 @@
+
+package repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import domain.Actor;
+import domain.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+	@Query("select u from User u where u.userAccount.id = ?1")
+	User findUserByUserAccountId(int UserAccountId);
+}
---
+++ Acme-Taxi/src/main/java/services/UserService.java
@@ -0,0 +1,49 @@
+
+package services;
+
+import java.util.Collection;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+import repositories.ActorRepository;
+import repositories.UserRepository;
+import security.LoginService;
+import security.UserAccount;
+import domain.Actor;
+import domain.User;
+
+@Service
+@Transactional
+public class UserService {
+
+	@Autowired
+	private UserRepository	userRepository;
+
+
+	public User findByPrincipal() {
+		User res;
+		UserAccount userAccount;
+		userAccount = LoginService.getPrincipal();
+		Assert.notNull(userAccount);
+		res = this.findUserByUserAccountId(userAccount.getId());
+		Assert.notNull(res);
+		return res;
+	}
+
+	public User findUserByUserAccountId(final int UserAccountId) {
+		return this.userRepository.findUserByUserAccountId(UserAccountId);
+	}
+
+	public Collection<User> findAll() {
+		return this.userRepository.findAll();
+	}
+
+	public User findOne(final int userId) {
+		final User u  = this.userRepository.findOne(userId);
+		return u;
+	}
+}
