8da34296966e1c20f50a64402f74b1de01c5e90a
Daniel <danielcarpioc@gmail.com>
2018-05-15 14:35:37 +0200

    Internacionalizado la búsqueda por username

--- Acme-Taxi/src/main/java/controllers/ActorController.java
+++ Acme-Taxi/src/main/java/controllers/ActorController.java
@@ -6,6 +6,8 @@ import java.util.Collection;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -23,6 +25,8 @@ public class ActorController extends AbstractController {
 
 	@Autowired
 	private ActorService	actorService;
+	@Autowired
+	private MessageSource	messageSource;
 
 
 	@RequestMapping("/list")
@@ -36,10 +40,30 @@ public class ActorController extends AbstractController {
 	String ajaxsearch(@RequestParam final String username) {
 		final Collection<Actor> actors = this.actorService.findByUsername(username);
 		String res = "";
-		List<Authority> temp;
+		String auth = null;
+		List<Authority> roles;
+		final String link = this.messageSource.getMessage("actor.link", null, LocaleContextHolder.getLocale());
+		final String linkEnd = this.messageSource.getMessage("actor.linkEnd", null, LocaleContextHolder.getLocale());
 		for (final Actor a : actors) {
-			temp = new ArrayList<>(a.getUserAccount().getAuthorities());
-			res += "<tr><td>" + temp.get(0) + "</td><td>" + a.getUserAccount().getUsername() + "</td><td>" + a.getName() + "</td><td>" + a.getSurname() + "</td><td>" + a.getId() + "</td></tr>";
+			roles = new ArrayList<>(a.getUserAccount().getAuthorities());
+			switch (roles.get(0).getAuthority()) {
+			case "ADMIN":
+				auth = this.messageSource.getMessage("actor.admin", null, LocaleContextHolder.getLocale());
+				break;
+			case "USER":
+				auth = this.messageSource.getMessage("actor.user", null, LocaleContextHolder.getLocale());
+				break;
+			case "DRIVER":
+				auth = this.messageSource.getMessage("actor.driver", null, LocaleContextHolder.getLocale());
+				break;
+			case "SPONSOR":
+				auth = this.messageSource.getMessage("actor.sponsor", null, LocaleContextHolder.getLocale());
+				break;
+			case "MECHANIC":
+				auth = this.messageSource.getMessage("actor.mechanic", null, LocaleContextHolder.getLocale());
+				break;
+			}
+			res += "<tr><td>" + auth + "</td><td>" + a.getUserAccount().getUsername() + "</td><td>" + a.getName() + "</td><td>" + a.getSurname() + "</td><td>" + link + a.getId() + linkEnd + "</td></tr>";
 		}
 		return res;
 	}
