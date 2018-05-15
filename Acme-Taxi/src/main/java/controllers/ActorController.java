eac2964376b7d6b75135751f61fefd284bea8d30
Daniel <danielcarpioc@gmail.com>
2018-05-15 15:19:41 +0200

    Botón de ver perfil en la búsqueda por usuario

--- Acme-Taxi/src/main/java/controllers/ActorController.java
+++ Acme-Taxi/src/main/java/controllers/ActorController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,6 +19,11 @@ import org.springframework.web.servlet.ModelAndView;
 import security.Authority;
 import services.ActorService;
 import domain.Actor;
+import domain.Admin;
+import domain.Driver;
+import domain.Mechanic;
+import domain.Sponsor;
+import domain.User;
 
 @Controller
 @RequestMapping("/actor")
@@ -66,5 +72,24 @@ public class ActorController extends AbstractController {
 			res += "<tr><td>" + auth + "</td><td>" + a.getUserAccount().getUsername() + "</td><td>" + a.getName() + "</td><td>" + a.getSurname() + "</td><td>" + link + a.getId() + linkEnd + "</td></tr>";
 		}
 		return res;
+	}
+
+	@RequestMapping("/display")
+	public ModelAndView display(@RequestParam final int actorId) {
+		final Actor a = this.actorService.findOne(actorId);
+		Assert.notNull(a);
+		if (a instanceof Admin)
+			return new ModelAndView("redirect:/admin/display.do?adminId=" + actorId);
+		else if (a instanceof Mechanic)
+			return new ModelAndView("redirect:/mechanic/display.do?mechanicId=" + actorId);
+		else if (a instanceof User)
+			return new ModelAndView("redirect:/user/display.do?userId=" + actorId);
+		else if (a instanceof Sponsor)
+			return new ModelAndView("redirect:/sponsor/display.do?sponsorId=" + actorId);
+		else if (a instanceof Driver)
+			return new ModelAndView("redirect:/driver/display.do?driverId=" + actorId);
+		else
+			throw new IllegalArgumentException();
+
 	}
 }
