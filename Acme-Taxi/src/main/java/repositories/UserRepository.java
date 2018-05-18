41ad6de694d7eff3dc1a96f8b238ad91fbc55447
Pablo Narváez <peluconus@hotmail.com>
2018-05-18 16:34:52 +0200

    Repositorio y Servicio de User

--- Acme-Taxi/src/main/java/repositories/UserRepository.java
+++ Acme-Taxi/src/main/java/repositories/UserRepository.java
@@ -1,6 +1,10 @@
 
 package repositories;
 
+import java.util.Collection;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -12,4 +16,16 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 
 	@Query("select u from User u where u.userAccount.id = ?1")
 	User findUserByUserAccountId(int UserAccountId);
+
+	@Query("select distinct a.attendants from User u join u.announcements a where u.id = ?1")
+	Page<User> findUsersReviewable(int userId, Pageable pageable);
+
+	@Query("select distinct a.attendants from User u join u.announcements a where u.id = ?1")
+	Collection<User> findUsersReviewable(int userId);
+
+	@Query("select u from User u join u.reviews r where r.creator.id = ?1")
+	Collection<User> findUserWithReviewByUserId(int userId);
+
+	@Query("select u from User u join u.reviews r where r.id = ?1")
+	User findUserByReviewId(int reviewId);
 }
