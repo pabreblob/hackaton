5176c71ee8fce40fbdac22a80c3c6d6acc8a85f1
Adrián Cantón Fernández <adrcanfer@gmail.com>
2018-06-05 20:42:18 +0200

    Corrección error despliegue

--- Despliegue/Create-Acme-Taxi.sql
+++ Despliegue/Create-Acme-Taxi.sql
@@ -1,3 +1,18 @@
+﻿start transaction;
+create database `Acme-Taxi`;
+
+use `Acme-Taxi`;
+
+create user 'acme-user'@'%' identified by password '*4F10007AADA9EE3DBB2CC36575DFC6F4FDE27577';
+create user 'acme-manager'@'%' identified by password '*FDB8CD304EB2317D10C95D797A4BD7492560F55F';
+
+grant select, insert, update, delete 
+	on `Acme-Taxi`.* to 'acme-user'@'%';
+grant select, insert, update, delete, create, drop, references, index, alter, 
+        create temporary tables, lock tables, create view, create routine, 
+        alter routine, execute, trigger, show view
+    on `Acme-Taxi`.* to 'acme-manager'@'%';
+
 -- MySQL dump 10.13  Distrib 5.5.29, for Win64 (x86)
 --
 -- Host: localhost    Database: Acme-Taxi
@@ -986,3 +1001,4 @@ UNLOCK TABLES;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2018-06-04 11:36:21
+commit;
